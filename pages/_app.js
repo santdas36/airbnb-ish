170daf95f47a604320943e3a401c8855743cf285
DASH SANTOSH S <santdas36@gmail.com>
2021-08-05 10:21:42 +0530

    add clarity

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,13 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
+      <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "7v7x1x83wa");
+      </script>
     </>
   );
 }
