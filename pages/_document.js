1e31a18764f270a6a752469843be6718493df354
DASH SANTOSH S <santdas36@gmail.com>
2021-08-05 12:23:32 +0530

    fix clarity

--- pages/_document.js
+++ pages/_document.js
@@ -8,13 +8,11 @@ class MyDocument extends Document {
 
   render() {
     const setInitialTheme = `
-    <script type="text/javascript">
       (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
       })(window, document, "clarity", "script", "7v7x1x83wa");
-    </script>
     `;
     return (
       <Html>
